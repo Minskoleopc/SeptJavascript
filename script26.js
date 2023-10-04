45073df86b6f2f056280c42f5b12f2915e53e4d9
unknown <Minskoleopc>
2023-10-04 06:27:40 -0700

    adde

--- script26.js
+++ script26.js
@@ -11,14 +11,14 @@ let students = [
         firstName:"poorva",
         lastName:"vyas",
         skills:["python","javascript","django"],
-        age:33
+        age:32
 
     },
     {
         firstName:"sarika",
         lastName:"pansare",
         skills:["python","cypress","playwright"],
-        age:33
+        age:31
     }
 
 ]
